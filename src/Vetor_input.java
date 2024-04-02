3dd30330e0e36a1d1d2e0d50ef78e4feca91063e
Alisson <140769066+alvesgc@users.noreply.github.com>
2024-04-02 19:53:33 -0300

    Aula sobre vetor via innput

---
+++ src/Vetor_input.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Vetor_input {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite o tamanho do vetor");
+        int n = sc.nextInt();
+        int vetor[] = new int [n];
+        System.out.println("Digite os elementos que pertencem a esse vetor");
+        for(int i = 0; i < n; i++) {
+            vetor[i] = sc.nextInt();
+        }
+        System.out.println("Impressão de elementos...");
+        for(int i = 0; i <  vetor.length; i++){
+            System.out.println(vetor[i] + "");
+        }
+    }
+}
+
