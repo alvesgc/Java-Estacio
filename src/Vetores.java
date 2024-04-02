654007f94b8b6a610bb6a044fcb79086260c270e
Alisson <140769066+alvesgc@users.noreply.github.com>
2024-04-02 19:45:25 -0300

    Aula sobre vetores

---
+++ src/Vetores.java
@@ -0,0 +1,13 @@
+public class Vetores {
+    public static void main(String [] args){
+        int v[] = {1,2,3,4,5,6,7,8,9};
+        for(int i = 0; i<v.length;i++){
+            System.out.println(v[i]+ "");
+        }
+        System.out.println();
+                Integer v2[] ={1,2,3,4,5,6,7,8,9};
+        for(int i = 0; i< v2.length; i++) {
+            System.out.println(v2[i] + "");
+        }
+    }
+}
