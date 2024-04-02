164b3d2a5c900e95f23a773c3b7c9c23c74f8063
Alisson <140769066+alvesgc@users.noreply.github.com>
2024-04-02 20:26:40 -0300

    Aula sobre matriz

---
+++ src/Matriz_cinema.java
@@ -0,0 +1,20 @@
+public class Matriz_cinema {
+    public static void main(String[] args){
+        int [][] m = new int[4][];
+        m[0] = new int [2];
+        m[1] = new int [4];
+        m[2] = new int [1];
+        m[3] = new int [10];
+        for(int i = 0; i < m.length; i++) {
+            for(int j = 0; j<m[i].length;j++){
+                m[i][j] = i+j;
+            }
+        }
+        for(int i = 0; i<m.length; i++) {
+            for (int j = 0; j < m[i].length; j++) {
+                System.out.print(m[i][j] + "");
+            }
+            System.out.println();
+        }
+    }
+}
